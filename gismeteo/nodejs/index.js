e0611744a8bee5954a77d71574ff9227a528330e
Ra <tryampampam@mail.ru>
2022-06-06 10:11:01 +0300

    Доработка

--- gismeteo/nodejs/index.js
+++ gismeteo/nodejs/index.js
@@ -25,17 +25,26 @@ for (var i=0; i<dirVal.length; i++) {
     }
 }*/
 
-const setGisInfo=()=>{
-  const myIP=execSync("curl -s ifconfig.co").toString().slice(0, -1);
-  const myCityRes=execSync("curl -s -H 'X-Gismeteo-Token: "+configs.gisToken+"' 'https://api.gismeteo.net/v2/search/cities/?ip="+myIP+"'").toString().slice(0, -1);
-  //console.log('myCity',myCity);
+const setGisInfo=async ()=>{
+  const myIP=execSync("curl -s ifconfig.co").toString().slice(0, -1),
+        rExistsIP=await redis.client.exists('gisM_city_'+myIP);
+  let myCityRes;
+  if (rExistsIP===0) {
+    myCityRes=execSync("curl -s -H 'X-Gismeteo-Token: "+configs.gisToken+"' 'https://api.gismeteo.net/v2/search/cities/?ip="+myIP+"'").toString().slice(0, -1);
+    redis.client.set('gisM_city_'+myIP, myCityRes);
+    redis.client.expire('gisM_city_'+myIP, configs.redis.expireForIP);//установка времени действия кэша
+  }
+  else {
+    myCityRes=await redis.client.get('gisM_city_'+myIP);
+  }
+  const myCityParse=JSON.parse(myCityRes);
+  //console.log('myCityParse',myCityParse);
   //const myCityRes=`{"meta":{"message":"","code":"200"},"response":{"district":{"name":"Воронежская область","nameP":"в Воронежской области"},"id":5026,"sub_district":{"name":"Воронеж (городской округ)","nameP":"в Воронеже"},"url":"\/weather-voronezh-5026\/","nameP":"в Воронеже","name":"Воронеж","kind":"M","country":{"name":"Россия","code":"RU","nameP":"в России"}}}`;
-  const gisRes=execSync("curl -s -H 'X-Gismeteo-Token: "+configs.gisToken+"' 'https://api.gismeteo.net/v2/weather/current/4368/?lang=ru'").toString().slice(0, -1);
+  const gisRes=execSync("curl -s -H 'X-Gismeteo-Token: "+configs.gisToken+"' 'https://api.gismeteo.net/v2/weather/current/"+myCityParse.response.id+"/?lang=ru'").toString().slice(0, -1);
   //console.log(gisRes);
   //const gisRes=`{"meta":{"message":"","code":"200"},"response":{"precipitation":{"type_ext":null,"intensity":0,"correction":null,"amount":0,"duration":0,"type":0},"pressure":{"h_pa":997,"mm_hg_atm":748,"in_hg":39.3},"humidity":{"percent":86},"icon":"d","gm":3,"wind":{"direction":{"degree":310,"scale_8":8},"speed":{"km_h":4,"m_s":1,"mi_h":2}},"cloudiness":{"type":0,"percent":21},"date":{"UTC":"2022-06-05 07:00:00","local":"2022-06-05 10:00:00","time_zone_offset":180,"hr_to_forecast":null,"unix":1654412400},"radiation":{"uvb_index":null,"UVB":null},"city":4368,"kind":"Obs","storm":false,"temperature":{"comfort":{"C":13.5,"F":56.3},"water":{"C":14.5,"F":58.1},"air":{"C":13.5,"F":56.3}},"description":{"full":"Ясно"}}}`;
   //fs.writeFileSync("./cashe/gisRes.json", gisRes);
-  const gisParse=JSON.parse(gisRes),
-        myCityParse=JSON.parse(myCityRes);
+  const gisParse=JSON.parse(gisRes);
   //составляем файл с текстом для отображения
   const gmCode={
           1:'Нет заметных возмущений',
@@ -74,7 +83,7 @@ const setGisInfo=()=>{
   redis.client.expire('gisMeteoInfo', (configs.counMSupd/1000+10));//установка времени действия кэша
 
   execSync('cp -f "'+path.join(path.dirname(__dirname),'icons',gisParse.response.icon+'.png')+'" "'+path.join(__dirname,'cashe','icon.png')+'"');
-  //fs.writeFileSync("./cashe/icon.txt", path.join(path.dirname(__dirname),'icons',gisParse.response.icon+'.png'));
+  //fs.writeFileSync("./cashe/icon.txt", path.join(path.dirname(__dirname),'icons',gisParse.response.icon+'.png'));*/
 }
 
 redis.client.connect().then(() => {
